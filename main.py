9647847a7efd14dda18fe86c5159bba1ec7f9efe
Zobelli <bozelli222gabriel@gmail.com>
2023-08-16 00:09:14 -0300

    implementação do gradiente descendente

--- main.py
+++ main.py
@@ -2,19 +2,47 @@ import pandas as pd
 import numpy as np
 
 def dotProduct(x,w,b):
-  s = np.matmul(x,w) + b
-  return s
+  z = np.matmul(x,w) + b
+  return z
 
 def activationFunction(x):
-  y = 1/(1+np.exp(-(x)))
-  return y
+  a = 1/(1+np.exp(-(x)))
+  return a
 
-def costFunction(X,YM):
-  J = 1/numberInputs*(
-    sum(sum((X-YM)**2)
-  ))
+def derivativeFunction(x):
+  return activationFunction(x)*(
+    1-activationFunction(x))
+
+def neuronOutput(x,w,b):
+  z = dotProduct(x,w,b)
+  a = activationFunction(z)
+  return a
+
+def costFunction(a,y):
+  c = (sum((a-y)**2))
+  return c
+
+def gradientVector(a1,a2,z,y):
+  gc = a1*derivativeFunction(z)*2*(a2-y)
+  return gc
+
+def neuralTraining(x,y):
+  w = np.array()
+  bias = np.array(numberLayers)
+  w.append(np.zeros(numberInputs,numberNeurons))
+  w.append(np.zeros(numberNeurons,numberOutputs))
+  steps = 10
+  for i in range(steps):
+    z = dotProduct(x,w,bias)
+    a1 = neuronOutput(x,w[i],bias)
+    a2 = neuronOutput(a1,w[i+1],bias)
+    gc = gradientVector(a1,a2,z,y)
+    w += gc
+  return w
+
 
 numberInputs = 0
 numberOutputs = 0
+numberLayers = 0
 numberNeurons = 0
 
