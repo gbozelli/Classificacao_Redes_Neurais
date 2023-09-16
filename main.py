7e63ac010e7c6fc9b0256c1419550361a806b732
Zobelli <bozelli222gabriel@gmail.com>
2023-09-16 17:03:14 -0300

    Modleo da Rede Neural modificado

--- main.py
+++ main.py
@@ -1,72 +1,111 @@
-import pandas as pd
 import numpy as np
-from numpy import transpose as t
+import random as rd
 from std import rand_cluster
-from matplotlib import pyplot as plt
 
-def dotProduct(x,w,b):
-  z = np.matmul(x,w) + b
-  return np.array(z)
+class Rede_Neural(object):
 
-def activationFunction(x):
-  a = 1/(1+np.exp(-1*(x)))
-  return np.array(a)
+  def __init__(self, camadas):
+    '''Inicializa a rede neural através da criação das matrizes
+    de pesos e da matriz de viéses'''
+    w = []
+    b = []
+    self.n_camadas = len(camadas)-1
+    for i in range(self.n_camadas): 
+      w.append(np.random.rand(
+        camadas[i],camadas[i+1]))
+      b.append(np.random.rand(
+        1,camadas[i+1]))
+    self.w = w
+    self.b = b
+    self.n_entradas = camadas[0]
+    
+  def produto_matricial(self, x, w, b):
+    z = np.array(np.matmul(x,w) + b)
+    return z
+  
+  def funcao_de_ativacao(self, z):
+    a = np.array((1/(1+np.exp(-1*(z)))))
+    return a
+  
+  def funcao_derivada(self, z):
+    return self.funcao_de_ativacao(z)*(
+    1-self.funcao_de_ativacao(z))
 
-def derivativeFunction(x):
-  return activationFunction(x)*(
-    1-activationFunction(x))
+  def funcao_de_custo(self, y_calculado, y_previsto):
+    return (y_calculado - y_previsto)
 
-def neuronOutput(x,w,b):
-  z = dotProduct(x,w,b)
-  a = activationFunction(z)
-  return a
+  def saida(self, x, w, b):
+    s = self.funcao_de_ativacao(
+      self.produto_matricial(x,w,b))
+    return s
+  
+  def descida_de_gradiente(self, x, y, iteracoes, taxa_de_aprendizado):
+    soma_b = [np.zeros(b.shape) for b in self.b]
+    soma_w = [np.zeros(w.shape) for w in self.w]
+    for i in range(iteracoes):
+      rd.shuffle(x)
+      delta_b, delta_w = self.retropropagacao(x,y)
+      soma_b = [nb+dnb for nb, dnb in zip(soma_b, delta_b)]
+      soma_w = [nw+dnw for nw, dnw in zip(soma_w, delta_w)]
+    self.w = [w-(taxa_de_aprendizado/self.n_entradas)*nw
+                      for w, nw in zip(self.w, soma_w)]
+    self.b = [b-(taxa_de_aprendizado/self.n_entradas)*nb
+                      for b, nb in zip(self.b, soma_b)]
 
-def costFunction(a,y):
-  c = (sum((a-y)**2))
-  return c
+  def retropropagacao(self,x,y):
+    delta_b = [np.zeros(b.shape) for b in self.b]
+    delta_w = [np.zeros(w.shape) for w in self.w]
+    e, entradas,produtos = x, [x], []
+    for i in range(self.n_camadas):
+      p = np.dot(e, self.w[i])+self.b[i]
+      produtos.append(p)
+      e = self.funcao_de_ativacao(p)
+      entradas.append(e)
+    delta = self.funcao_de_custo(entradas[-1], y) * \
+            self.funcao_de_ativacao(produtos[-1])
+    delta_b[-1] = delta
+    delta_w[-1] = np.dot(entradas[-2].transpose(), delta)
+    for l in range(2, self.n_camadas):
+      p = produtos[-l]
+      e = self.funcao_de_ativacao(p)
+      delta = np.dot(delta, self.w[-l+1].transpose()) * e
+      delta_b[-l] = delta
+      delta_w[-l] = np.dot(delta, entradas[-l-1].transpose())
+    return (delta_b, delta_w)
 
-def gradientVector(activation,activation2,z,y):
-  a,a2,b = [], [], []
-  a.append(activation)
-  a2.append(activation2)
-  b.append(z)
-  a = np.array(t(a))
-  b = np.array(b)
-  a2 = np.array(a2)
-  gc = np.matmul(a,(derivativeFunction(b)*(a2-y)))
-  return gc
+  def treinamento(self, X,y,taxa_de_aprendizado,iteracoes):
+    for x in zip(X):
+      self.descida_de_gradiente(x,y,iteracoes,taxa_de_aprendizado)
+    saida = self.saida(x,self.w[0],self.b[0])
+    for i in range(1,self.n_camadas):
+      saida = self.saida(saida,self.w[i],self.b[i])
+    return saida
+  
+  def normalizar(self, dados):
+    self.min = np.min(dados)
+    self.max = np.min(dados)
+    dados_norm = (dados-np.min(dados))/(
+      np.max(dados)-np.min(dados))
+    return dados_norm
+  
+  def renormalizar(self, dados_norm):
+    min = self.min
+    max = self.max
+    dados = dados_norm*(max-min)+min
+    return dados
 
-def neuralTraining(x,y):
-  w1 = np.array(np.random.random(
-    (n_inputs,n_neurons)))
-  w2 = np.array(np.random.random(
-    (n_neurons,n_outputs)))
-  bias1 = np.array(n_neurons)
-  bias2 = np.array(n_outputs)
-  steps = 100
-  for i in range(steps):
-    a = neuronOutput(x,w1,bias1)
-    z = dotProduct(a,w2,bias2)
-    activation = neuronOutput(x,w1,bias1)
-    activation2 = neuronOutput(activation,w2,bias2)
-    gc = gradientVector(activation,activation2,z,y)
-    w2 += gc
-    ym = activation
-    z = dotProduct(x,w1,bias1)
-    activation = x
-    activation2 = neuronOutput(activation,w1,bias1)
-    gc = gradientVector(activation,activation2,z,ym)
-    w1 += gc
-  return w1,w2,bias1,bias2
 
-def test(x,w1,w2,y):
-  neuronOutput
-
-n_inputs = 2
-n_outputs = 2
-n_neurons = 4
-
-x1 = np.array(np.transpose(rand_cluster(2,13,0.75,0.75)))
-w1, w2,b1,b2 = neuralTraining(x1[0],x1[1])
-nx = neuronOutput(neuronOutput(x1,w1,b1),w2,b2)
-print(nx)
+N = 100
+center = 5
+sigma = 20
+sigma_n = 0.1
+a = 0.5
+b = 0.9
+X = center+sigma*np.random.rand(N,1)
+n = sigma_n*np.random.randn(N,1)
+Y = a*X+b+n
+points = [X,Y]
+points = np.transpose(points)[0]
+r = Rede_Neural([2,10,2])
+saida = r.treinamento(points,[a,b],0.001,1000)
+print(saida)
--- neural.py
+++ neural.py
@@ -7,51 +7,34 @@ class Rede_Neural(object):
   def __init__(self, camadas):
     '''Inicializa a rede neural através da criação das matrizes
     de pesos e da matriz de viéses'''
-    w = []
-    b = []
     self.n_camadas = len(camadas)-1
-    for i in range(self.n_camadas): 
-      w.append(np.random.rand(
-        camadas[i],camadas[i+1]))
-      b.append(np.random.rand(
-        1,camadas[i+1]))
+    w = [np.random.rand(camadas[i],camadas[i+1]) for i in range(self.n_camadas)]
+    b = [np.random.rand(1,camadas[i+1]) for i in range(self.n_camadas)]
     self.w = w
     self.b = b
     self.n_entradas = camadas[0]
     
   def produto_matricial(self, x, w, b):
-    '''Produto entre a matriz de pesos e o 
-    vetor de entrada, somada com o viés.'''
     z = np.array(np.matmul(x,w) + b)
     return z
   
   def funcao_de_ativacao(self, z):
-    '''Função de ativação do neurônio. No nosso
-    caso, a função sigmoidal, multiplicada por uma constante
-    para registrar melhor a saída (coeficientes de uma função)'''
     a = np.array((1/(1+np.exp(-1*(z)))))
     return a
   
   def funcao_derivada(self, z):
-    '''Função derivada da de ativação. É utilizada no
-    cálculo da retropropagação do erro'''
     return self.funcao_de_ativacao(z)*(
     1-self.funcao_de_ativacao(z))
 
   def funcao_de_custo(self, y_calculado, y_previsto):
-    '''Função básica para o algoritmo de regressão'''
     return (y_calculado - y_previsto)
 
   def saida(self, x, w, b):
-    '''Saída de uma camada de neurônios'''
     s = self.funcao_de_ativacao(
       self.produto_matricial(x,w,b))
     return s
   
   def descida_de_gradiente(self, x, y, iteracoes, taxa_de_aprendizado):
-    '''Função que, através de um número definido de iterações,
-    calcula o mínimo global da função de custo, e encontra
-    os melhores valores para os pesos e viéses'''
     soma_b = [np.zeros(b.shape) for b in self.b]
     soma_w = [np.zeros(w.shape) for w in self.w]
     for i in range(iteracoes):
@@ -65,9 +48,6 @@ class Rede_Neural(object):
                       for b, nb in zip(self.b, soma_b)]
 
   def retropropagacao(self,x,y):
-    '''Realiza a retroprogação do erro. Observe que, apesar de
-    ser definida de maneira iterativa, pode-se também utilizar um
-    algoritmo recursivo para os cálculos'''
     delta_b = [np.zeros(b.shape) for b in self.b]
     delta_w = [np.zeros(w.shape) for w in self.w]
     e, entradas,produtos = x, [x], []
@@ -88,47 +68,30 @@ class Rede_Neural(object):
       delta_w[-l] = np.dot(delta, entradas[-l-1].transpose())
     return (delta_b, delta_w)
 
+  def treinamento(self, x,y,taxa_de_aprendizado,iteracoes):
+    self.descida_de_gradiente(x,y,iteracoes,taxa_de_aprendizado)
+  
   def normalizar(self, dados):
+    '''Normaliza os dados dos conjuntos entre 0 e 1'''
     self.min = np.min(dados)
     self.max = np.min(dados)
     dados_norm = (dados-np.min(dados))/(
       np.max(dados)-np.min(dados))
     return dados_norm
   
-  def renormalizar(self, dados_norm):
+  def desnormalizar(self, dados_norm):
+    '''Renormaliza os dados dos conjuntos entre o valor
+    máximo e o valor mínimo'''
     min = self.min
     max = self.max
     dados = dados_norm*(max-min)+min
     return dados
 
-  def treinamento(self, X,y,taxa_de_aprendizado,iteracoes):
-    '''Treina a rede neural. O conjunto dos dados é injetado na rede
-    a cada iteração de forma aleatória, para evitar overfitting'''
-    self.descida_de_gradiente(X,y,iteracoes,taxa_de_aprendizado)
-    saida = self.saida(X,self.w[0],self.b[0])
+  def teste(self, x):
+    saida = self.saida(x,self.w[0],self.b[0])
     for i in range(1,self.n_camadas):
       saida = self.saida(saida,self.w[i],self.b[i])
     return saida
-  
-N = 100
-center = 5
-sigma = 20
-sigma_n = 0.1
-a = 0.5
-b = 1.4
-X = center+sigma*np.random.rand(N,1)
-n = sigma_n*np.random.randn(N,1)
-Y = a*X+b+n
-points = []
-for x, y in zip(X,Y):
-  points.append(x)
-  points.append(y)
-points = np.transpose(points)[0]
-r = Rede_Neural([2*N,10,2])
-points = r.normalizar(points)
-print(points)
-saida = r.treinamento(points,[a,b],0.001,1000)
-print(saida)
-saida = r.renormalizar(saida)
-print(saida)
+
+r = Rede_Neural([764,20,10,10])
 
